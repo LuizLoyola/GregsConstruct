7b21be7a3db0e89b8dd5bb2ee3e6837dfd5fb010
Luiz Loyola <luizpedro@mloyola.com.br>
2022-01-10 01:47:38 -0300

    greg made me do it

--- src/main/java/gregsconstruct/FluidTemp.java
+++ src/main/java/gregsconstruct/FluidTemp.java
@@ -6,7 +6,7 @@ import gregtech.api.unification.material.type.IngotMaterial;
 import gregtech.api.unification.material.type.Material;
 
 @IMaterialHandler.RegisterMaterialHandler
-public class FluidTemp {
+public class FluidTemp implements IMaterialHandler {
     static {
         for (Material mat : Material.MATERIAL_REGISTRY)
             if (mat instanceof DustMaterial && (mat instanceof IngotMaterial || mat.hasFlag(DustMaterial.MatFlags.SMELT_INTO_FLUID))) {
@@ -15,5 +15,17 @@ public class FluidTemp {
                 else
                     ((DustMaterial) mat).setFluidTemperature(500);
             }
+    }
+
+    @Override
+    public void onMaterialsInit() {
+        /*
+         * idk what to do here, so ima leave it blank
+         * GregTech asked me to do it:
+         *
+         * > [Client thread/ERROR] [gregtech]: Failed to load material handler class gregsconstruct.FluidTemp from GregsConstruct-1.12.2-1.0.2-UNOFFICIAL.jar: class does not implement IMaterialHandler
+         * > [Client thread/ERROR] [gregtech]: Consult the mod author for fix on his side.
+         *
+         */
     }
 }
